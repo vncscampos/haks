47db2279f12473dedf2fb092eec3e2d36b76edfc
vnscampos <viniolic2018@gmail.com>
2021-07-14 11:26:35 -0300

    alterando arquivo scannet.sh

--- scannet.sh
+++ scannet.sh
@@ -9,6 +9,6 @@ then
 else
     for ip in {1..254};
     do
-        hping3 -S -p $2 -c 1 $1.$ip 2> /dev/null | grep "flags=SA" | cut -d " " -f2;
+        hping3 -S -p $2 -c 1 $1.$ip 2> /dev/null | grep "flags=SA" | cut -d " " -f 2;
     done
 fi
