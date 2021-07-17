391a8dc9dec8c6051519fecea49a53c1d3ab778c
vnscampos <viniolic2018@gmail.com>
2021-07-17 13:55:53 -0300

    criando um portscan com python

---
+++ python/portscan.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import socket, sys
+
+ip = raw_input("Digite o IP: ")
+
+for port in range(1, 65535):
+	mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	if (mysocket.connect_ex((ip, port)) == 0):
+		print "Porta", port, "[ABERTA]"
+		mysocket.close()
