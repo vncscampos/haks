cdd76fbd170624ace1d0b8cfcc33f239abe83b4a
viniolic <viniciuscampos@tokenlab.com.br>
2022-04-03 14:50:01 -0300

    dnsbrute.py

--- dns/dns_resolver.py
+++ dns/dns_resolver.py
@@ -1,13 +1,22 @@
 import dns.resolver
+import sys
 
-# url = input('Digite o domínio: ')
+try:
+    url = sys.argv[1]
+    name_file = sys.argv[2]
+    wordlist = open(name_file, 'r').readlines()
+except:
+    print('Usage: python3 dnsbrute.py domain wordlist.txt')
+    sys.exit()
 
 resolver = dns.resolver.Resolver()
 
-try:
-    results = resolver.resolve('bancocn.com', 'A')
-    for res in results:
-        print(res)
-    
-except Exception as err:
-    print('Error:', err)
+for subdomain in wordlist:
+    try:
+        target = "{}.{}".format(subdomain.rstrip('\n'), url)
+        results = resolver.resolve(target, 'A')
+        if(results):
+            for res in results:
+                print("subdomain: {} --> {}" .format(target, res))
+    except Exception as err:
+        pass
